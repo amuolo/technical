e16cfb3f3a8d61af0d6d95292444302e924f9279
amuolo <and.muolo@gmail.com>
2024-03-01 11:01:54 +0100

    some lines

--- include/utils.h
+++ include/utils.h
@@ -8,8 +8,41 @@
 #ifndef INCLUDE_UTILS_H_
 #define INCLUDE_UTILS_H_
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <functional>
 
+using namespace std;
 
+template<class T>
+std::vector<T> parse_input_line()
+{
+    string s;
+    T item;
+    getline( cin, s );
+    stringstream stream(s);
+    std::vector<T> input;
+    
+    while(stream >> item)        
+        input.push_back(item);
+   
+    return input;
+}
 
+auto print = [](std::vector<int> arr)
+{
+    for (auto a : arr)
+        std::cout << a << ' ';
+};
+
+auto print = [](std::vector<string> arr)
+{
+    for (auto a : arr)
+	std::cout << a << endl;
+};
 
 #endif /* INCLUDE_UTILS_H_ */
