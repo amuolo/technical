74d42d43278f7025362c75c083a5502567e6d9d4
amuolo <and.muolo@gmail.com>
2024-03-22 23:11:12 +0100

    calculator methods

---
+++ Tech/utils/Calculator.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef INCLUDE_UTILS_CALCULATOR_H_
+#define INCLUDE_UTILS_CALCULATOR_H_
+
+#include "StandardLibs.h"
+
+namespace tech 
+{
+    int get_greater_common_divisor(int aIn, int bIn) {
+        int a = aIn > bIn ? aIn : bIn;
+        int b = aIn > bIn ? bIn : aIn;
+        while (b != 0) {
+            auto residual = a % b;
+            a = b;
+            b = residual;
+        }
+        return a;
+    }
+}
+
+#endif
