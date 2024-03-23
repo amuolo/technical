90bdf5092ec28dab6653f2c41ec2450c824e1304
amuolo <and.muolo@gmail.com>
2024-03-23 12:26:24 +0100

    add primes in calculator + tests

--- Tech/test/CalculatorTest.cpp
+++ Tech/test/CalculatorTest.cpp
@@ -2,13 +2,24 @@
 
 #define BOOST_TEST_MODULE Calculator
 
-#include "../utils/StandardLibs.hpp"
-#include "../utils/Arithmetics.hpp"
-#include "../utils/calculator.hpp"
+#include "../utils/Calculator.hpp"
 
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(simple_gcd) {
+BOOST_AUTO_TEST_CASE(gcd) {
 
-	BOOST_TEST(3 == 3);
+	BOOST_TEST(tech::get_greater_common_divisor(3, 9) == 3);
+
+	BOOST_TEST(tech::get_greater_common_divisor(5, 3) == 1);
+
+	BOOST_TEST(tech::get_greater_common_divisor(100, 1) == 1);
+
+	BOOST_TEST(tech::get_greater_common_divisor(15, 30) == 15);
+
+	BOOST_TEST(tech::get_greater_common_divisor(15, 21) == 3);
+}
+
+BOOST_AUTO_TEST_CASE(primes) {
+
+	BOOST_TEST(tech::get_primes(100).size() == 25);
 }
--- Tech/utils/Calculator.hpp
+++ Tech/utils/Calculator.hpp
@@ -7,15 +7,31 @@
 
 namespace tech 
 {
-    int get_greater_common_divisor(int aIn, int bIn) {
-        int a = aIn > bIn ? aIn : bIn;
-        int b = aIn > bIn ? bIn : aIn;
+    template <class T>
+    T get_greater_common_divisor(T aIn, T bIn) requires(std::is_integral_v<T>) {
+        auto a = aIn > bIn ? aIn : bIn;
+        auto b = aIn > bIn ? bIn : aIn;
         while (b != 0) {
             auto residual = a % b;
             a = b;
             b = residual;
         }
         return a;
+    }
+
+    template <class T>
+    std::vector<T> get_primes(T max) requires(std::is_integral_v<T>) {
+        auto res = std::vector<T>{ 2,3,5,7,11,13,17,19,23,29,31,37 };
+        for (T n = 41; n <= max; n++) {
+            bool ok = true;
+            T sqrtn = std::sqrt(n);
+            for (T prime : res) {
+                if (n % prime == 0) { ok = false; break; }
+                if (prime > sqrtn) break;
+            }
+            if (ok) res.push_back(n);
+        }
+        return res;
     }
 }
 
