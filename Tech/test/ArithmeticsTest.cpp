d552198d34e877060e89ade691f4732bfeb3be81
amuolo <and.muolo@gmail.com>
2024-03-23 18:32:59 +0100

    more arithmetics

--- Tech/test/ArithmeticsTest.cpp
+++ Tech/test/ArithmeticsTest.cpp
@@ -3,44 +3,56 @@
 #define BOOST_TEST_MODULE Arithmetics
 
 #include "../utils/StandardLibs.hpp"
-#include "../utils/InputOutput.hpp"
 #include "../utils/Arithmetics.hpp"
 
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(simple_pair_sum) {
-	auto res = std::make_pair(2, 2) + std::make_pair(1, 3);
+BOOST_AUTO_TEST_CASE(pair_sum) {
+	auto res1 = std::make_pair(2, 2) + std::make_pair(1, 3);
 
-	BOOST_TEST(res.first == 3);
+	BOOST_TEST(res1.first == 3);
+	BOOST_TEST(res1.second == 5);
 
-	BOOST_TEST(res.second == 5);
+	auto res2 = std::make_pair(2.3, 'x') + std::make_pair(4.5, 'y');
+
+	BOOST_TEST(res2.first == 6.8);
+	BOOST_TEST(res2.second == -15);
 }
 
-BOOST_AUTO_TEST_CASE(complex_pair_sum) {
-	auto res = std::make_pair(2.3, 'x') + std::make_pair(4.5, 'y');
+BOOST_AUTO_TEST_CASE(tuple_sum) {
+	auto res1 = std::tuple(2, 2.3, 'a') + std::tuple(3, 1.1, 'b');
 
-	BOOST_TEST(res.first == 6.8);
+	BOOST_TEST(std::get<0>(res1) == 5);
+	BOOST_TEST(std::get<1>(res1) == 3.4);
+	BOOST_TEST(std::get<2>(res1) == -61);
 
-	BOOST_TEST(res.second == -15);
-}
-
-BOOST_AUTO_TEST_CASE(simple_tuple_sum) {
-	auto res = std::tuple(2, 2.3, 'a') + std::tuple(3, 1.1, 'b');
-
-	BOOST_TEST(std::get<0>(res) == 5);
-
-	BOOST_TEST(std::get<1>(res) == 3.4);
-
-	BOOST_TEST(std::get<2>(res) == -61);
-}
-
-BOOST_AUTO_TEST_CASE(char_pointer_sum) {
 	std::string a = "hello", b = " ", c = "world", d = "!";
-
-	auto res = std::tuple(a.data(), c.data()) + std::tuple(b.data(), d.data());
-
-	auto str = std::string(std::get<0>(res)) + std::string(std::get<1>(res));
+	auto res2 = std::tuple(a.data(), c.data()) + std::tuple(b.data(), d.data());
+	auto str = std::string(std::get<0>(res2)) + std::string(std::get<1>(res2));
 
 	BOOST_TEST(str == "hello world!");
+}
+
+BOOST_AUTO_TEST_CASE(vector_sum) {
+	
+	using cxint = std::complex<int>;
+
+	std::vector<cxint> a = { cxint(1, 2), cxint(2, -4) };
+	std::vector<cxint> b = { cxint(1, -2), cxint(-5, 2) };
+
+	auto r = a * b;
+
+	BOOST_TEST(r == cxint(-21, -20));
+	BOOST_TEST(a * 2 == std::vector<cxint>({ cxint(2, 4), cxint(4, -8) }));
+}
+
+BOOST_AUTO_TEST_CASE(vector_dotproduct) {
+
+	BOOST_TEST(2 == 2);
+}
+
+BOOST_AUTO_TEST_CASE(vector_kronecker_product) {
+
+	BOOST_TEST(2 == 2);
 }
 
--- Tech/utils/Arithmetics.hpp
+++ Tech/utils/Arithmetics.hpp
@@ -5,8 +5,12 @@
 
 #include "StandardLibs.hpp"
 
+#include <boost/lexical_cast.hpp>
+#include <boost/type_traits.hpp>
+
 template <class T1, class T2>
-inline std::string tech_sum(const T1& a, const T2& b) requires(std::is_same_v<T1, char*>) {
+inline std::string tech_sum(const T1& a, const T2& b) requires(std::is_same_v<T1, char*>) 
+{
 	auto i = a ? std::string(a) : "";
 	auto j = b ? std::string(b) : "";
 	auto r = i + j;
@@ -14,31 +18,173 @@ inline std::string tech_sum(const T1& a, const T2& b) requires(std::is_same_v<T1
 }
 
 template <class T1, class T2>
-inline T1 tech_sum(const T1& a, const T2& b) requires(!std::is_same_v<T1, char*> && !std::is_same_v<T2, char*>) {
+inline T1 tech_sum(const T1& a, const T2& b) requires(!std::is_same_v<T1, char*> && !std::is_same_v<T2, char*>) 
+{
 	return a + b;
 }
 
-template <class T1, class T2, class W1, class W2>
-inline auto operator+ (const std::pair<T1, T2>& a, const std::pair<W1, W2>& b) {
+/**********************************************************/
+/*                         Tuple                          */
+/**********************************************************/
 
+/***************/
+/*     Sum     */
+/***************/
+
+template <class T1, class T2, class W1, class W2>
+inline auto operator + (const std::pair<T1, T2>& a, const std::pair<W1, W2>& b) 
+{
 	return std::pair(tech_sum(a.first, b.first), 
 					 tech_sum(a.second, b.second));
 };
 
 template <class T1, class T2, class W1, class W2>
-inline auto operator+ (const std::tuple<T1, T2>& a, const std::tuple<W1, W2>& b) {
-
+inline auto operator + (const std::tuple<T1, T2>& a, const std::tuple<W1, W2>& b) 
+{
 	return std::tuple(tech_sum(std::get<0>(a), std::get<0>(b)), 
 					  tech_sum(std::get<1>(a), std::get<1>(b)));
 };
 
 template <class T1, class T2, class T3, class W1, class W2, class W3>
-inline auto operator+ (const std::tuple<T1, T2, T3>& a, const std::tuple<W1, W2, W3>& b) {
-
+inline auto operator + (const std::tuple<T1, T2, T3>& a, const std::tuple<W1, W2, W3>& b) 
+{
 	return std::tuple(tech_sum(std::get<0>(a), std::get<0>(b)), 
 					  tech_sum(std::get<1>(a), std::get<1>(b)), 
 					  tech_sum(std::get<2>(a), std::get<2>(b)));
 }
+
+/**********************************************************/
+/*                     STL Containers                     */
+/**********************************************************/
+
+/***************/
+/*   Product   */
+/***************/
+
+template <class T, class Q>
+inline T operator * (const std::vector<T>& a, const std::vector<Q>& b)
+{
+	if (a.size() != b.size())
+		throw std::exception("vector size mismatch");
+
+	T r = boost::lexical_cast<T>("0.0");
+
+#pragma omp declare reduction( + : T : omp_out=omp_out+omp_in) initializer(omp_priv(omp_orig))
+#pragma omp declare reduction( + : std::complex<T> : omp_out=omp_out+omp_in) initializer(omp_priv(omp_orig))
+#pragma omp simd reduction(+:r)
+	for (size_t i = 0; i < a.size(); i++) {
+		if constexpr (boost::is_complex<T>::value)
+			r += std::conj(a[i]) * b[i];
+		else
+			r += a[i] * b[i];
+	}
+	
+	return r;
+}
+
+template <class T, class Q>
+std::vector<T> kronecker(const std::vector<T>& lhs, const std::vector<Q>& rhs)
+{
+	std::vector<T> r(lhs.size() * rhs.size(), boost::lexical_cast<T>("0.0"));
+	
+#pragma omp simd collapse(2)
+	for (size_t i = 0; i < lhs.size(); i++)
+		for (size_t j = 0; j < rhs.size(); j++)
+			r[i * rhs.size() + j] = lhs[i] * rhs[j];
+
+	return r;
+}
+
+template <class T, class S>
+inline std::vector<T>& operator *= (std::vector<T>& v, const S& a)
+{
+#pragma omp simd
+	for (size_t i = 0; i < v.size(); i++)
+		v[i] *= a;
+	return v;
+}
+
+template <class T, class S>
+inline std::vector<T> operator * (std::vector<T> v, const S& a) 
+{
+	return v *= a;
+}
+
+
+template <class T, class S>
+inline std::vector<T> operator * (const S& a, std::vector<T> v) 
+{
+	return v *= a;
+}
+
+/****************/
+/*   Division   */
+/****************/
+
+template <class T>
+inline std::vector<T>& operator /= (std::vector<T>& v, const T& a)
+{
+#pragma omp simd
+	for (size_t i = 0; i < v.size(); i++)
+		v[i] /= a;
+	return v;
+}
+
+
+template <class T>
+inline std::vector<T> operator / (std::vector<T> v, const T& a)
+{
+	return v /= a;
+}
+
+/***************/
+/*     Sum     */
+/***************/
+
+template <class T>
+inline std::vector<T>& operator += (std::vector<T>& lhs, const std::vector<T>& rhs) requires(std::is_arithmetic_v<T>)
+{
+	if (lhs.size() != rhs.size())
+		throw std::exception("vector size mismatch");
+
+#pragma omp simd
+	for (size_t i = 0; i < lhs.size(); i++)
+		lhs[i] += rhs[i];
+
+	return lhs;
+}
+
+
+template <class T>
+inline std::vector<T> operator + (std::vector<T> lhs, const std::vector<T>& rhs)
+{
+	return lhs += rhs;
+}
+
+/****************/
+/*  Difference  */
+/****************/
+
+template <class T>
+inline std::vector<T>& operator -= (std::vector<T>& lhs, const std::vector<T>& rhs) 
+{
+	if (lhs.size() != rhs.size())
+		throw std::exception("vector size mismatch");
+
+#pragma omp simd
+	for (size_t i = 0; i < lhs.size(); i++)
+		lhs[i] -= rhs[i];
+
+	return lhs;
+}
+
+
+template <class T>
+inline std::vector<T> operator - (std::vector<T> lhs, const std::vector<T>& rhs)
+{
+	return lhs -= rhs;
+}
+
 
 #endif
 
